b1877c28dc79cc9b0f55ad1268fe6a3235107872
guihassen <guilherme.hassenpflug@sou.inteli.edu.br>
2026-03-18 10:50:53 -0300

    docs: modifica nav-bar

--- case_agrominas/docusaurus.config.js
+++ case_agrominas/docusaurus.config.js
@@ -89,20 +89,20 @@ const config = {
           alt: "My Site Logo",
           src: "img/logo.png",
         },
-        items: [
-          {
-            type: "docSidebar",
-            sidebarId: "tutorialSidebar",
-            position: "left",
-            label: "Tutorial",
-          },
-          { to: "/blog", label: "Blog", position: "left" },
-          {
-            href: "https://github.com/facebook/docusaurus",
-            label: "GitHub",
-            position: "right",
-          },
-        ],
+        // items: [
+        //   {
+        //     type: "docSidebar",
+        //     sidebarId: "tutorialSidebar",
+        //     position: "left",
+        //     label: "Tutorial",
+        //   },
+        //   { to: "/blog", label: "Blog", position: "left" },
+        //   {
+        //     href: "https://github.com/facebook/docusaurus",
+        //     label: "GitHub",
+        //     position: "right",
+        //   },
+        // ],
       },
       footer: {
         style: "dark",
